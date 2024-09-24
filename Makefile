7569b2df9bb3bb06845bef3a9aedeeba0f1f8ac7
Frey Alfredsson <freysteinn@freysteinn.com>
2024-09-24 16:30:21 +0200

    Moving neighsnoopd to its new home
    This commit prepares the project for its new home, 1984's Github[1] page.
    
    [1] https://github.com/1984hosting/neighsnoopd
    
    Signed-off-by: Frey Alfredsson <freysteinn@freysteinn.com>

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 # SPDX-License-Identifier: GPL-2.0-or-later
-# SPDX-FileCopyrightText: 2024 1984 <1984@1984.is>
-# SPDX-FileCopyrightText: 2024 Freyx Solutions <frey@freyx.com>
+# SPDX-FileCopyrightText: 2024 - 1984 Hosting Company <1984@1984.is>
+# SPDX-FileCopyrightText: 2024 - Freyx Solutions <frey@freyx.com>
 # SPDX-FileContributor: Freysteinn Alfredsson <freysteinn@freysteinn.com>
 # SPDX-FileContributor: Julius Thor Bess Rikardsson <juliusbess@gmail.com>
 
--- neighsnoopd.c
+++ neighsnoopd.c
@@ -1,6 +1,6 @@
 /* SPDX-License-Identifier: GPL-2.0-or-later */
-/* SPDX-FileCopyrightText: 2024 1984 <1984@1984.is> */
-/* SPDX-FileCopyrightText: 2024 Freyx Solutions <frey@freyx.com> */
+/* SPDX-FileCopyrightText: 2024 - 1984 Hosting Company <1984@1984.is> */
+/* SPDX-FileCopyrightText: 2024 - Freyx Solutions <frey@freyx.com> */
 /* SPDX-FileContributor: Freysteinn Alfredsson <freysteinn@freysteinn.com> */
 /* SPDX-FileContributor: Julius Thor Bess Rikardsson <juliusbess@gmail.com> */
 
@@ -67,7 +67,7 @@ const char *argp_program_version = "neighsnoopd v0.9\n"
     "git commit: " GIT_COMMIT;
 
 const char *argp_program_bug_address =
-        "https://www.github.com/freysteinn/neighsnoopd";
+        "https://github.com/1984hosting/neighsnoopd";
 
 const char argp_program_doc[] =
     "Listens for ARP replies and adds the neighbor to the Neighbors table.\n";
